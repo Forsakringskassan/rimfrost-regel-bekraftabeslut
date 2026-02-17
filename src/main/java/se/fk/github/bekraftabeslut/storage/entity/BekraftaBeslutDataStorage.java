28b89e3df6cbf383c9d780f8e3f4ad05c062f61d
Lars Persson <254442132+larpersso@users.noreply.github.com>
2026-02-17 16:25:10 +0100

    fix: Use rimfrost-framework-storage
    This commit adds support for persistent storage
    provided by rimfrost-framework-storage. It also
    introduces thread synchronization in order to
    ensure that multiple threads does not write to
    shared data at the same time.

---
+++ src/main/java/se/fk/github/bekraftabeslut/storage/entity/BekraftaBeslutDataStorage.java
@@ -0,0 +1,7 @@
+package se.fk.github.bekraftabeslut.storage.entity;
+
+import se.fk.rimfrost.framework.regel.manuell.storage.entity.RegelManuellDataStorage;
+
+public class BekraftaBeslutDataStorage extends RegelManuellDataStorage
+{
+}
